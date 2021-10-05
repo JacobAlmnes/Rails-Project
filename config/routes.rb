09a1bceace75406c76f7ac373203fc62c582d936
Jacob Almnes <jacob.almnes@gmail.com>
2021-10-05 17:06:18 +0200

    Added seed-data and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/movies" => "movies#index"
+  get "/movies/:id"=> "movies#show", as:"movie"
+  root "movies#index"
+  # get "/" => "movies#index", Kan også gjøre slik#
 end
